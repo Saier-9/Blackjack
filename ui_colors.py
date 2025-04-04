44e4784f8c2266ba2f723f463a1337523e0d8ad1
Saier_9 <saier.dev@gmail.com>
2025-04-04 14:03:22 -0700

    change colors here

---
+++ ui_colors.py
@@ -0,0 +1,8 @@
+# Main Background Color
+bg_color = "#009a4d"
+
+# Main Button & Bet Entry Color
+accent_color = "#00d169"
+
+# Chooses Which Card Back Is Used From /assets/backs/[color]_back.png
+card_back_color = "blue"
