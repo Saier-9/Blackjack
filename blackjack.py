07956a54aa306746be2aa5bae62c8f6132f805aa
Saier_9 <saier.dev@gmail.com>
2025-04-04 14:02:46 -0700

    added delay when laying out cards

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import font
 from PIL import Image, ImageTk
-import random, time, math
+import random, math, ui_colors
+
+# dealer got 17 I got 16 says dealer had 16 but he still won
 
 class BlackjackGame:
     def __init__(self, root:tk.Tk):
@@ -9,14 +11,15 @@ class BlackjackGame:
         self.root.title("Blackjack")
         self.balance = 100
         self.bet = 0
+        self.player_soft = ""
+        self.dealer_soft = ""
         self.deck = self.create_deck()
         self.player_hand = []
         self.dealer_hand = []
         self.card_images = []
-        self.soft = ""
-        self.bg_color = "#009a4d"
-        self.accent_color = "#00d169"
-        self.card_back_color = "blue"
+        self.bg_color = ui_colors.bg_color
+        self.accent_color = ui_colors.accent_color
+        self.card_back_color = ui_colors.card_back_color
 
         self.setup_ui()
         
@@ -56,7 +59,7 @@ class BlackjackGame:
         self.stand_button = tk.Button(self.button_frame, text="Stand", command=self.stand, state=tk.DISABLED, background=self.accent_color, activebackground=self.accent_color)
         self.stand_button.pack(side=tk.LEFT, padx=2)
 
-        self.double_down_button = tk.Button(self.button_frame, text="Double Down", command=self.double_down, state=tk.DISABLED, background=self.accent_color, activebackground=self.accent_color)
+        self.double_down_button = tk.Button(self.button_frame, text="Double Down", command=lambda:self.hit(True), state=tk.DISABLED, background=self.accent_color, activebackground=self.accent_color)
         self.double_down_button.pack(side=tk.LEFT, padx=2)
 
         self.player_total_label = tk.Label(self.root, text="Your total: 0", background=self.bg_color, fg="white")
@@ -86,59 +89,70 @@ class BlackjackGame:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         return [{'value': value, 'suit': suit} for suit in suits for value in values] * 4
 
-    def deal_card(self, hand):
+    def deal_card(self, hand, face_down=False):
         card = random.choice(self.deck)
         self.deck.remove(card)
         hand.append(card)
-        self.clear_table()
-        self.add_player_cards()
-        self.add_dealer_cards()
+        self.add_card_ui(card, hand, face_down)
 
     def calculate_score(self, hand):
         score = 0
         aces = 0
+        soft = ""
         for card in hand:
             if card['value'] in ['J', 'Q', 'K']:
                 score += 10
             elif card['value'] == 'A':
                 aces += 1
                 score += 11
-                self.soft = "Soft "
+                soft = "Soft "
             else:
                 score += int(card['value'])
         
         while score > 21 and aces:
             score -= 10
             aces -= 1
-            self.soft = ""
+            soft = ""
         
+        if hand == self.player_hand:
+            self.player_soft = soft
+        else:
+            self.dealer_soft = soft
+
         return score
 
-    def clear_table(self):
+    def clear_table_ui(self):
         for widget in self.player_frame.winfo_children():
             widget.destroy()
         for widget in self.dealer_frame.winfo_children():
             widget.destroy()
 
-    def add_player_cards(self):
-        for card in self.player_hand:
-            self.draw_card(self.player_frame, card)
-
-        self.player_total_label.config(text=f"Your total: {self.soft}{self.calculate_score(self.player_hand)}")
+    def reveal_dealer_card(self):
+        self.dealer_frame.winfo_children()[0].destroy()
+        card = self.dealer_hand[0]
+        card_face_image = Image.open(f"assets/cards/{card['value']}{card['suit']}.png")
+        card_face_image = card_face_image.resize((55, 80))
+        card_face = ImageTk.PhotoImage(card_face_image)
+        self.card_images.append(card_face)
+        card_label = tk.Label(self.dealer_frame, image=card_face, background=self.bg_color)
+        card_label.pack(side=tk.LEFT, padx=2, before=self.dealer_frame.winfo_children()[0] if self.dealer_frame.winfo_children() else None)
         
-    def add_dealer_cards(self, reveal_dealer=False):
-        for i, card in enumerate(self.dealer_hand):
-            if i == 0 and not reveal_dealer:
-                self.draw_card(self.dealer_frame, None)
-            else:
-                self.draw_card(self.dealer_frame, card)
+        dealer_score = self.calculate_score(self.dealer_hand)
+        self.dealer_total_label.config(text=f"Dealer's total: {self.dealer_soft}{dealer_score}")
 
-        if reveal_dealer:
-            self.dealer_total_label.config(text=f"Dealer's total: {self.soft}{self.calculate_score(self.dealer_hand)}")
+    def add_card_ui(self, card, hand, face_down=False):
+        if hand == self.player_hand:
+            self.draw_card_ui(self.player_frame, card)
+            player_score = self.calculate_score(self.player_hand)
+            self.player_total_label.config(text=f"Your total: {self.player_soft}{player_score}")
         else:
-            self.dealer_total_label.config(text="Dealer's total: ?")
-
-    def draw_card(self, frame, card):
+            if face_down:
+                self.draw_card_ui(self.dealer_frame, None)
+                self.dealer_total_label.config(text="Dealer's total: ?")
+            else:
+                self.draw_card_ui(self.dealer_frame, card)
+                
+    def draw_card_ui(self, frame, card):
         if card:
             card_face_image = Image.open(f"assets/cards/{card["value"]}{card["suit"]}.png")
             card_face_image = card_face_image.resize((55, 80))
@@ -172,14 +186,14 @@ class BlackjackGame:
         self.stand_button.config(state=tk.NORMAL)
         self.double_down_button.config(state=tk.NORMAL)
         self.result_label.config(text="")
-        self.clear_table()
+        self.clear_table_ui()
         
         self.player_hand = []
         self.dealer_hand = []
         self.deal_card(self.player_hand)
-        self.deal_card(self.player_hand)
-        self.deal_card(self.dealer_hand)
-        self.deal_card(self.dealer_hand)
+        self.root.after(300, lambda:self.deal_card(self.player_hand))
+        self.root.after(600, lambda:self.deal_card(self.dealer_hand, True))
+        self.root.after(900, lambda:self.deal_card(self.dealer_hand))
         if self.calculate_score(self.player_hand) == 21:
             self.balance += math.floor(self.bet * 2.5)
             self.result_label.config(text="Blackjack! You win!")
@@ -193,9 +207,6 @@ class BlackjackGame:
         self.hit_button.config(state=tk.DISABLED)
         self.stand_button.config(state=tk.DISABLED)
         self.double_down_button.config(state=tk.DISABLED)
-        
-    def double_down(self):
-        self.hit(True)
 
     def hit(self, double=False):
         if double:
@@ -218,9 +229,16 @@ class BlackjackGame:
             self.stand()
 
     def stand(self):
-        while self.calculate_score(self.dealer_hand) < 17:
-            self.deal_card(self.dealer_hand)
+        def dealer_draw(delay=300):
+            if self.calculate_score(self.dealer_hand) < 17:
+                self.root.after(delay, lambda: [self.deal_card(self.dealer_hand), dealer_draw(delay + 300)])
+            else:
+                self.check_winner()
+
+        self.reveal_dealer_card()
+        dealer_draw()
         
+    def check_winner(self):
         player_score = self.calculate_score(self.player_hand)
         dealer_score = self.calculate_score(self.dealer_hand)
         
@@ -251,9 +269,6 @@ class BlackjackGame:
         self.hit_button.config(state=tk.DISABLED)
         self.stand_button.config(state=tk.DISABLED)
         self.double_down_button.config(state=tk.DISABLED)
-        self.clear_table()
-        self.add_player_cards()
-        self.add_dealer_cards(True)
 
 if __name__ == "__main__":
     root = tk.Tk()
